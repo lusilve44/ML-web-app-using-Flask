1817c865970e2dc47b7db61a4ede96af8c803cb8
Luisina Silvestre <luisina.silvestre0914@gmail.com>
2022-08-20 03:25:02 +0000

    avance3

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/inicio')
 
 def index():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 loaded_model = pickle.load(open('coffee_model.pkl', 'rb')) # rb: read binary
 
